60a6b8331ab68d20b08e00d36890ee709aae9843
Aleksandra Dworak <aaleksandra.dworak@gmail.com>
2019-05-17 11:21:03 +0200

    country search engine - redux connecion

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from 'redux';
+
+import reducers from '../reducers/index';
+import DevTools from '../DevTools'
+
+
+
+const store = createStore(
+    reducers,
+    DevTools.instrument()
+);
+
+
+
+export default store;
